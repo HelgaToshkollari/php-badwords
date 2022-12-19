b8edad3234b0ffe8ae88de54bced8d1d70090fab
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-12-19 18:33:42 +0100

    adds changes on form

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Form</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
     integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -12,21 +12,24 @@
 </head>
 <body>
     <main class="container">
-        <form action="sent-comment.php" method="POST" class="p-4">
-            <div class="form-group mb-2">
-                <label for="">Name</label>
-                <input type="text" class="form-control"  placeholder="Name" name="userName">
+        <div class="row justify-content-center"> 
+            <div class="col-sm-6">
+                <form action="sent-comment.php" method="POST" class="p-4">
+                    <div class="form-group mb-2">
+                        <label for="">Word to censor</label>
+                        <input type="text" class="form-control"  placeholder="..." name="userWord">
+                    </div>
+                    <div class="form-group mb-2">
+                        <label for="">Paragraph</label>
+                        <textarea class="form-control" placeholder="..." name="userText"></textarea>
+                    </div>
+                    <button class="btn btn-dark">Submit</button>
+                </form>
+
             </div>
-            <div class="form-group mb-2">
-              <label for="">Surname</label>
-              <input type="text" class="form-control" placeholder="Surname" name="userSurname">
-            </div>
-            <div class="form-group mb-2">
-                <label for="">Comment</label>
-                <textarea class="form-control" name="userComment"></textarea>
-            </div>
-            <button class="btn btn-dark">Submit</button>
-        </form>
+        </div>
+
+        
 
     </main>
     
--- sent-comment.php
+++ sent-comment.php
@@ -1,7 +1,46 @@
 <?php
 
-$name = $_POST["userName"];
-$surname =  $_POST["userSurname"];
-$comment =  $_POST["userComment"];
+$word = $_POST["userWord"];
+$text =  $_POST["userText"];
+
+
+
+
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Results</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+    integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+
+</head>
+<body>
+    <main class="container ">
+        <div class="row p-4 d-flex justify-content-center text-center ">
+            <div class="col">
+            <h2>This is your original paragraph: </h2>  
+                <div>
+                    <h4><?php echo $text ?></h4>
+                </div>
+        
+
+            </div>
+            <div class="col">
+                <h2> This is the censored version: </h2>
+                    <div>
+                        <h4><?php echo str_replace($word, "***", $text) ?></h4>
+                    </div>    
+            </div>
+        </div>
+    </main>
+    
+</body>
+</html>
